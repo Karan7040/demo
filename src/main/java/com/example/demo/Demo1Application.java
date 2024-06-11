5eb89dbf347ddc01c1c2c8c617b3b7e420e58f88
Karan Vallakati <karan.vallakati@colruytgroup.com>
2024-06-11 16:23:51 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+// TODO : employee service to bve injcted
+        //Component , Service, Controller, ControllerAdvice, Configuration , Repository
+        // application.properties/application.yaml
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
